523c14cca054795b0302af486b4259d55d6038e2
BrintaMidya <brintamidya@gmail.com>
2020-11-06 11:25:20 +0530

    Updates

---
+++ precache-manifest.9c8e57386ad50642781d9b6aad263028.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a0ad91e261c3064dda8f1c0b6c68f75",
+    "url": "/picture-perfect/index.html"
+  },
+  {
+    "revision": "d387aa0fadfc1a8596b8",
+    "url": "/picture-perfect/static/css/main.275947aa.chunk.css"
+  },
+  {
+    "revision": "73faa413f6b9e6c3baef",
+    "url": "/picture-perfect/static/js/2.bc68cb7b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/picture-perfect/static/js/2.bc68cb7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d387aa0fadfc1a8596b8",
+    "url": "/picture-perfect/static/js/main.b083a61b.chunk.js"
+  },
+  {
+    "revision": "f736b67cdf90e5df7b58",
+    "url": "/picture-perfect/static/js/runtime-main.9500d618.js"
+  }
+]);
